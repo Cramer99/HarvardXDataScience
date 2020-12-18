f79fd74a41210a6818540132e5742c45e552f69f
Cramer99 <55208700+Cramer99@users.noreply.github.com>
2020-12-17 21:28:11 -0500

    Add files via upload

---
+++ section_3_indexing.R
@@ -0,0 +1,80 @@
+# defining murder rate as before
+murder_rate <- murders$total / murders$population * 100000
+# creating a logical vector that specifies if the murder rate in that state is less than or equal to 0.71
+index <- murder_rate <= 0.71
+# determining which states have murder rates less than or equal to 0.71
+murders$state[index]
+# calculating how many states have a murder rate less than or equal to 0.71
+sum(index)
+
+ # creating the two logical vectors representing our conditions
+west <- murders$region == "West"
+safe <- murder_rate <= 1
+# defining an index and identifying states with both conditions true
+index <- safe & west
+murders$state[index]
+sum(index)
+
+x <- c(FALSE, TRUE, FALSE, TRUE, TRUE, FALSE)
+
+which(x)    # returns indices that are TRUE
+
+# to determine the murder rate in Massachusetts we may do the following
+index <- which(murders$state == "Massachusetts")
+index
+murder_rate[index]
+
+# to obtain the indices and subsequent murder rates of New York, Florida, Texas, we do:
+index <- match(c("New York", "Florida", "Texas"), murders$state)
+index
+murders$state[index]
+murder_rate[index]
+
+x <- c("a", "b", "c", "d", "e")
+y <- c("a", "d", "f")
+y %in% x
+
+# to see if Boston, Dakota, and Washington are states
+c("Boston", "Dakota", "Washington") %in% murders$state
+
+# Store the murder rate per 100,000 for each state, in `murder_rate`
+murder_rate <- murders$total / murders$population * 100000
+
+# Store the `murder_rate < 1` in `low` 
+low <- murder_rate < 1
+
+# Get the indices of entries that are below 1
+which(low)
+
+# Names of states with murder rates lower than 1
+murders$state[low]
+
+# Create a vector ind for states in the Northeast and with murder rates lower than 1. 
+ind <- low & murders$region == "Northeast"
+# Names of states in `ind` 
+murders$state[ind]
+
+# Compute the average murder rate using `mean` and store it in object named `avg`
+avg <- mean(murder_rate)
+
+# How many states have murder rates below avg ? Check using sum 
+sum(murder_rate < avg)
+
+# Store the 3 abbreviations in a vector called `abbs` (remember that they are character vectors and need quotes)
+abbs <- c("AK", "MI", "IA")
+# Match the abbs to the murders$abb and store in ind
+ind <- match(abbs, murders$abb)
+# Print state names from ind
+murders$state[ind]
+
+# Store the 5 abbreviations in `abbs`. (remember that they are character vectors)
+abbs <- c("MA", "ME", "MI", "MO", "MU")
+
+# Use the %in% command to check if the entries of abbs are abbreviations in the the murders data frame
+abbs %in% murders$abb
+
+# Use the `which` command and `!` operator to find out which index abbreviations are not actually part of the dataset and store in `ind`
+ind <- which(!abbs%in%murders$abb)
+# Names of abbreviations in `ind`
+abbs[ind]
+sum(ind)
